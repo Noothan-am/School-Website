23e62d9770b9a440a8af618095a25a6283214e52
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2024-01-26 14:33:05 +0530

    adds about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Admissions from "./pages/Admissions";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Admissions />
     </div>
   );
 }
---
+++ client/src/pages/Admissions.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Admissions() {
+  return (
+    <div>
+      <table class="table table-bordered">
+        <thead className="table-bordered">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Fee</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">P-Exam</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">Lab</th>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">Lab</th>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">CDC</th>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">TOTAL</th>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@fat</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Admissions;
