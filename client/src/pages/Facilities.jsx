9e13cd910af65b97f6b51fa44e24b2fd0e6d7a22
MD SAMEER <razer@MDs-MacBook-Air.local>
2024-02-03 00:19:31 +0530

    Add facilities desc

--- client/src/pages/Facilities.jsx
+++ client/src/pages/Facilities.jsx
@@ -23,22 +23,32 @@ function Facilities() {
         <div className={styles["content"]}>
           <div className="first-part">
             <Facility
-              heading="Dive In!"
-              description="Our modern pool offers a refreshing space for students to learn aquatic skills under expert supervision."
+              heading="Auditorium for Cultural Events"
+              description="Our auditorium serves as a hub for cultural events, performances, and gatherings. With state-of-the-art audio-visual facilities, it provides an ideal platform for students to showcase their talents in the performing arts and for the community to come together."
             />
             <Facility
-              heading="Tech-Driven Learning"
-              description="Experience interactive education with our smart classes, fostering a futuristic approach to learning and understanding."
+              heading="Science Laboratories"
+              description=" Explore the wonders of science in our advanced laboratories. Equipped with cutting-edge apparatus, our science labs provide students with hands-on experiences, fostering a love for scientific inquiry and discovery."
             />
           </div>
           <div className="second-part">
             <Facility
-              heading="Play, Compete, Thrive"
-              description="Explore our top-notch grounds and sports facilities, promoting teamwork, sportsmanship, and a passion for physical well-being."
+              heading="Library and Study Halls"
+              description="Our extensive library and quiet study halls offer a conducive atmosphere for academic pursuits. Students have access to a wide range of books, journals, and online resources to support their learning and research endeavors."
             />
             <Facility
-              heading="Nourish, Flourish"
-              description="Our food scheme ensures wholesome meals, laying the foundation for healthy living and optimal academic performance."
+              heading="Computer and Science Laboratories"
+              description="Explore the wonders of computers and science in our advanced laboratories. Equipped with cutting-edge apparatus, our science labs provide students with hands-on experiences, fostering a love for scientific inquiry and discovery."
+            />
+          </div>
+          <div className="second-part">
+            <Facility
+              heading="Digital Learning Center"
+              description="Embrace the future of education in our digital learning center. Equipped with the latest technology, students can engage in interactive and immersive learning experiences, enhancing their skills and knowledge in various subjects."
+            />
+            <Facility
+              heading="Multipurpose Sports Field and Indoor Badminton Court"
+              description="Our multipurpose sports field is designed to accommodate a variety of sports activities, from football and cricket to athletics. Our indoor badminton court is equipped with professional-grade flooring and lighting, providing the perfect environment for badminton enthusiasts."
             />
           </div>
         </div>
